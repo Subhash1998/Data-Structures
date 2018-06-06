9a98bc1d6511c36ec11d8254920dbd3bdcc33421
subhash <you@example.com>
2018-06-07 00:15:17 +0530

    doubly linked list

---
+++ doubly linked list/add_node_end.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+struct node
+{
+    int info;
+    //cannot declare back(it is a keyword) as variable
+    node *forw,*backw;
+}*start,*endd,*newptr,*ptr,*save;
+//cannot declare end(a keyword) as variable
+
+node *create_new_node(int n)
+{
+    ptr=new node;
+    ptr->info=n;
+    ptr->forw=ptr->backw=NULL;
+    return ptr;
+}
+
+void add_node_end(node *np)
+{
+    if(start==NULL)
+        start=endd=np;
+    else
+    {
+        np->backw=endd;
+        endd->forw=np;
+        endd=np;
+    }
+}
+
+void display(node *np)
+{
+    while(np!=NULL)
+    {
+        cout<<np->info<<"->";
+        np=np->forw;
+    }
+}
+
+int main()
+{
+    char ans='y';
+    int n;
+    start=endd=NULL;
+    while(ans=='y'||ans=='Y')
+    {
+        cout<<"\nEnter the item to be added :- ";
+        cin>>n;
+        cout<<"\nCreating new node";
+        newptr=create_new_node(n);
+        if(newptr!=NULL)
+        {
+            cout<<"\nNew node created";
+        }
+        else
+        {
+            cout<<"\nCannot create new node....Aborting!!!";
+            exit(1);
+        }
+        cout<<"\nAdding new node to linked list";
+        add_node_end(newptr);
+        cout<<"\nNew node added successfully!!!";
+        cout<<"\nThe new linked list is :- \n";
+        display(start);
+        cout<<"\nDo you wish to continue(y/n)? :- ";
+        cin>>ans;
+    }
+    return 0;
+}
