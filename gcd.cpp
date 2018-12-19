809b1438f25534c777b89495415b1b3f9c58158c
Subhash Kshatri <omkshatri98@gmail.com>
2018-12-19 16:47:01 +0530

    gcd of two numbers

---
+++ gcd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a,int b)
+{
+    cout<<a<<" "<<b<<"\n";
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+
+int main()
+{
+    int a=6,b=16;
+    int x=gcd(a,b);
+    cout<<x;
+    return 0;
+}
