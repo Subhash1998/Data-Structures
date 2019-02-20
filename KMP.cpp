52420582c8198eb522ee08a97061a44165e481ac
Subhash Kshatri <omkshatri98@gmail.com>
2019-02-20 11:13:45 +0530

    KMP algorithm

---
+++ KMP.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long 
+#define N 1000000
+
+int main()
+{
+    string txt,pat;
+    cin>>txt>>pat;
+    ll lps[N];
+    lps[0]=0;
+    ll i=1,j=0;
+    while(i<pat.length())
+    {
+        if(pat[i]==pat[j])
+        {
+            lps[i]=j+1;
+            j++;
+            i++;
+        }
+        else
+        {
+            if(j==0)
+            {
+                lps[i]=0;
+                i++;
+            }
+            else
+                j=lps[j-1];
+        }
+    }
+    i=0;
+    j=0;
+    while(i<txt.length())
+    {
+        if(txt[i]==pat[j])
+        {
+            i++;
+            j++;
+        }
+        if(j==pat.length())
+        {
+            cout<<"found at index = "<<i-j<<"\n";
+            j=lps[j-1];
+        }
+        else if(i<txt.length() && txt[i]!=pat[j])
+        {
+            if(j==0)
+                i++;
+            else
+                j = lps[j-1];
+        }
+    }
+    return 0;
+}
