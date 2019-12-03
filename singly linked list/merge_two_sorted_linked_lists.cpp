1de235332c113621c334fa686de502d2a661a0c1
Subhash Kshatri <omkshatri98@gmail.com>
2019-12-03 19:49:25 +0530

    added recursive method

--- singly linked list/merge_two_sorted_linked_lists.cpp
+++ singly linked list/merge_two_sorted_linked_lists.cpp
@@ -1,5 +1,6 @@
 //function only...
 
+//Method 1 (self-made)
 
 Node* sortedMerge(Node* head1,   Node* head2)
 {
@@ -49,4 +50,24 @@ Node* sortedMerge(Node* head1,   Node* head2)
         temp2->next = ptr2;
     }
     return head1;
+}
+
+//Method 2 (recursive:- Source geeksforgeeks)
+
+Node* sortedMerge(Node* head1,   Node* head2)
+{
+    if(head2==NULL)
+        return head1;
+        
+    if(head1==NULL)
+        return head2;
+    
+    if(head1->data <= head2->data)
+    {
+        head1->next = sortedMerge(head1->next,head2);
+        return head1;
+    }
+    
+    head2->next = sortedMerge(head1,head2->next);
+    return head2;
 }
